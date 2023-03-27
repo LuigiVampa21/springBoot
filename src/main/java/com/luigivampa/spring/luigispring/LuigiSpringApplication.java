c764707aa58ce6afcd62206bdf65d2629e66e05e
Nathan Teissier <nmteissier@gmail.com>
2023-03-27 16:02:00 +0200

    initial-commit

---
+++ src/main/java/com/luigivampa/spring/luigispring/LuigiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.luigivampa.spring.luigispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuigiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuigiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luigivampa/spring/luigispring/LuigiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigivampa.spring.luigispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuigiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
